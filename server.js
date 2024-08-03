5f80a90d104d2f9b885342d3d9f284269f2283db
piradite <veis2k15veis@gmail.com>
2024-08-03 02:24:07 +0200

    dwwd

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ HARDCOVER: -------
     `,
     [process.env.CODE16]: `
       <div style="width: 50vw; height: 50vh; padding: 10px;">
-        <img src="https://static.wikia.nocookie.net/disney/images/5/50/S1e4_mystery_shack.png/revision/latest?cb=20150714211909">
+        <img src="https://static.wikia.nocookie.net/disney/images/5/50/S1e4_mystery_shack.png">
       </div>
     `,
     [process.env.CODE17]: `
