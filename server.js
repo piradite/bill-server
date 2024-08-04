5b020b732b19160b163c85d1cb934676683de736
piradite <veis2k15veis@gmail.com>
2024-08-04 18:42:50 +0200

    wj

--- server.js
+++ server.js
@@ -204,6 +204,11 @@ HARDCOVER: -------
   </p>
   
     `,
+    [process.env.CODE21]: `
+    <div style="width: 50vw; height: 50vh; padding: 10px;">
+    <img src="https://media.discordapp.net/attachments/1268231029072597086/1269695562643406908/IMG_2952.png?ex=66b0ffa3&is=66afae23&hm=e7563a1638a2b5847ccbdaa2cd2c75799d3af508eebfc273ae7076e3335a52c6&=&format=webp&quality=lossless&width=771&height=660" style= position: absolute;>
+  </div>
+`
   };
 
   if (validCodes[normalizedCode]) {
